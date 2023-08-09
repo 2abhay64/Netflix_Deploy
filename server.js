30d34abdb5dea1d8674737870af9f33f67124e9c
2abhay64 <abhaysyadav2378@gmail.com>
2023-08-09 20:18:27 +0530

    final code
    mern aap deployment

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+const userRoutes = require("./routes/UserRoutes");
+const path = require('path')
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://127.0.0.1/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+//static file
+ app.use(express.static(path.join(__dirname, './netflix-ui/build')))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname,  './client/build/index.html'))
+})
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
